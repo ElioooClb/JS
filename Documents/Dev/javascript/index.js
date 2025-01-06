4ea0cf36e906f641da853caa94f6afd5996f2773
ElioooClb <collomb.eliot@gmail.com>
2025-01-06 11:38:13 +0100

    OnClick

--- Documents/Dev/javascript/index.js
+++ Documents/Dev/javascript/index.js
@@ -1,3 +1,11 @@
 // document.querySelector("h4").style.background = "red";
-const baliseTitreH4 = document.querySelector("h4")
-baliseTitreH4.style.background = "blue"
+// const baliseTitreH4 = document.querySelector("h4")
+// baliseTitreH4.style.background = "blue"
+const questionContainer = document.querySelector(".onClick");
+//console.log(questionContainer);
+
+
+questionContainer.addEventListener("click",()=>{
+//questionContainer.classList.add("questionOnClick");
+questionContainer.classList.toggle("questionOnClick");
+})
